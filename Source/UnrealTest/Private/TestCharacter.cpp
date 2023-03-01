b53048c8a4fbfd03d38057e0ee20099b5ffc6f31
kentery <2451777168@qq.com>
2023-03-01 20:36:12 +0800

    Added TestCharacter & Setup Input
    -Added Character class.
    -Added MoveForward/Turn input

---
+++ Source/UnrealTest/Private/TestCharacter.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ATestCharacter::ATestCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	SpringArmComp->SetupAttachment(RootComponent);
+
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
+	CameraComp->SetupAttachment(SpringArmComp);
+}
+
+// Called when the game starts or when spawned
+void ATestCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATestCharacter::MoveForward(float value)
+{
+	AddMovementInput(GetActorForwardVector(), value);
+}
+
+// Called every frame
+void ATestCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATestCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &ATestCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+}
+
---
+++ Source/UnrealTest/UnrealTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALTEST_API AUnrealTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
